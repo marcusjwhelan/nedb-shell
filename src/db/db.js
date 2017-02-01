30c28db07e5a2250fcabfab4c411b486d84729c9
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-02-01 10:27:58 -0600

    possibly should use functions not classes?

--- src/db/db.js
+++ src/db/db.js
@@ -1,4 +1,5 @@
 'use strict';
+
 module.exports = class db{
   
 };
--- src/db/insert.js
+++ src/db/insert.js
@@ -1,7 +1,23 @@
 'use strict';
  const db = require('./db'),
        nedb = require('nedb');
+
+class insert extends db{
+  
+};
  
- module.exports = class insert extends db{
+ module.exports.insert = insert;
    
- };
+// or would it be
+
+/*
+* const db = require('./db'),
+*       nedb = require('nedb');
+*
+*
+* const insert = () => {
+*
+* }
+*
+* module.exports.insert = insert;
+* */
