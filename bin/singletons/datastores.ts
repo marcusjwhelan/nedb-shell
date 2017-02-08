d4418f85c7cf0ee408471ab1397b91efaecd38bf
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-02-08 14:07:48 -0600

    some last second additions

--- bin/singletons/datastores.ts
+++ bin/singletons/datastores.ts
@@ -1,28 +1,5 @@
 import { Editable } from '../decorators/factories/editable';
 
-// To import this singleton instance use
-// import { stores } from 'this path';
-
-/*export class DATASTORES {
-  private _stores: Array<string> = [];
-
-  get prop(): Array<string>{
-    return this._stores;
-  }
-
-  @Editable(false)
-  addStore(store: string){
-    this._stores.push(store);
-  }
-
-  @Editable(false)
-  addStores(stores: Array<string>){
-    this._stores.push(...stores);
-  }
-}
-
-export let stores = new DATASTORES();*/
-
 export class DATASTORES {
   private static instance: DATASTORES;
   private _stores: Array<string> = [];
@@ -50,7 +27,3 @@ export class DATASTORES {
   }
 }
 
-/* AVOID */
-// To create a new class DATASTORES use
-// import { DATASTORES  } from 'this path';
-// let urVar = new DATASTORES();
--- bin/singletons/path.ts
+++ bin/singletons/path.ts
@@ -1,20 +1,5 @@
-// To import this singleton instance use
-// import { path } from 'this path';
 
 import { Editable } from '../decorators/factories/editable';
-/*export class PATH {
-  private _path: string = '';
-
-  get prop(): string{
-    return this._path;
-  }
-
-  set prop(path: string){
-    this._path = path;
-  }
-}
-
-export let path = new PATH();*/
 
 export class PATH {
   private static instance: PATH;
@@ -36,8 +21,3 @@ export class PATH {
     this._path = path;
   }
 }
-
-/* AVOID */
-// To create a new class PATH use
-// import { PATH } from 'this path';
-// let urVar = new PATH();
