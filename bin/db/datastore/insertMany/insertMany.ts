441f9316ffe11fb2490d2e016b669cd2685304c8
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-02-15 14:13:36 -0600

    added cb for commit many only for error chacking. If however the user wants to run a cb on each inserted document be my guest

--- bin/db/datastore/insertMany/insertMany.ts
+++ bin/db/datastore/insertMany/insertMany.ts
@@ -1,5 +1,14 @@
+import * as chalk from 'chalk';
+
+const insertManyCB = function ( err:Error, docs:{}[] ) {
+  if(err){
+    console.log(chalk.red(`${err}`));
+    console.log(JSON.stringify(docs,null,2));
+  }
+};
+
 export function InsertMany(newDocs:{}[], cb?:any):void{
   for(let item of newDocs ){
-    this.insert(item);
+    this.insert(item, (cb?cb:insertManyCB));
   }
 }
