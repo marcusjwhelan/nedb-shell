470fb9e01cc39694bc7d45b5841f057acaa41d60
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-02-17 09:29:17 -0600

    remove help

--- bin/Load.ts
+++ bin/Load.ts
@@ -1,7 +1,7 @@
 //const repl = require('repl');
 
 export function Load(repl:any){
-  for(let nedb_shell of ['db','help','show']){
+  for(let nedb_shell of ['db','show']){
     repl.context[nedb_shell] = require(`./${nedb_shell}`);
   }
 }
