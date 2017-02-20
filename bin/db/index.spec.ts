782afc7357859b2cc089a11a3f0dee07e171444d
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-02-20 15:55:37 -0600

    added mocha and chai. npm test to run tests. use .spec.ts files to write tests in.

---
+++ bin/db/index.spec.ts
@@ -0,0 +1,11 @@
+import * as db from './index';
+
+import { expect } from 'chai';
+import 'mocha';
+
+
+describe('Load DB object', () => {
+  it('should return an Object with certain properties', () => {
+    expect(db).to.have.all.keys('createDatastore','show','Help');
+  })
+});
