d664ffb7b5130b577e57e4271c1974d0ec751ccd
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-02-24 16:13:43 -0600

    ship it all? refactor after weekend

--- spec/db/index.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var db = require("../../bin/db/index");
-var chai_1 = require("chai");
-require("mocha");
-describe('Load DB object', function () {
-    it('should return an Object with certain properties', function () {
-        chai_1.expect(db).to.have.all.keys('createDatastore', 'show', 'Help');
-    });
-});
