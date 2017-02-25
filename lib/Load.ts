7dd4fe520a277442875cf626deb6633e8d95fcba
tsturzl <meatwad12>
2017-02-24 17:30:04 -0700

    include helper modules for the end user(lodash and moment)

--- lib/Load.ts
+++ lib/Load.ts
@@ -1,8 +1,13 @@
 //const repl = require('repl');
-const moment = require('moment')
+const moment = require('moment'),
+  lodash     = require('lodash');
 
 export function Load(repl:any){
   for(let nedb_shell of ['db','show']){
     repl.context[nedb_shell] = require(`./${nedb_shell}`);
   }
+
+  //Include helper modules
+  repl.context['_'] = lodash;
+  repl.context.moment = moment;
 }
